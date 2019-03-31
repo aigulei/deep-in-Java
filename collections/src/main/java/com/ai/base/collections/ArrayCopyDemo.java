437138ae250387c4004f1fbd7698ff40815c67ec
touchsky <touchjava@126.com>
2019-03-31 20:49:23 +0800

    init collections

---
+++ collections/src/main/java/com/ai/base/collections/ArrayCopyDemo.java
@@ -0,0 +1,16 @@
+package com.ai.base.collections;
+
+public class ArrayCopyDemo {
+    public static void main(String[] args) {
+        String[] strings1 = new String[]{"Hello","World"};
+        arrayCopy(strings1,strings1);
+
+        int value = Integer.MAX_VALUE;
+        System.out.println(value+1);
+        System.out.println(Integer.MIN_VALUE);
+    }
+
+    public static void arrayCopy(Object[] src,Object[] destination){
+
+    }
+}
